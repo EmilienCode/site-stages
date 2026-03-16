785d6c9bbe3f09472c67b5c234c4359aaf654a59
charlozorus <devinescharles@gmail.com>
2026-03-16 16:44:41 +0100

    oh he

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once __DIR__.'/vendor/autoload.php'; #Cette ligne charge automatiquement toutes les librairies installées avec Composer, notamment Twig.
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/templates'); #DIR  représente le dossier du fichier actuel donc cette ligne dit que les fichiers Twig sont dans le dossier templates
 $twig = new \Twig\Environment($loader);
