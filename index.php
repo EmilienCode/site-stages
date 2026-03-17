7852d458515da58e2420a884ee7e9b179943eb75
Emilien <emi.rsu45@gmail.com>
2026-03-17 10:33:36 +0100

    ajout du système d econnexion + le bouton admin qui apparait uniquement quand on est connecté

--- index.php
+++ index.php
@@ -1,25 +1,21 @@
 <?php
+session_start(); // 1. AJOUT : Obligatoire pour lire les infos de connexion
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require_once __DIR__.'/vendor/autoload.php'; #Cette ligne charge automatiquement toutes les librairies installées avec Composer, notamment Twig.
-$loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/templates'); #DIR  représente le dossier du fichier actuel donc cette ligne dit que les fichiers Twig sont dans le dossier templates
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/templates');
 $twig = new \Twig\Environment($loader);
-#Cette ligne crée l’environnement Twig.
 
-#C’est l’objet principal qui va :
-
-    #lire les templates
-
-    #interpréter Twig
-
-    #générer le HTML final
-
+// 2. LA LIGNE MAGIQUE À PLACER ICI :
+// Elle donne accès à la variable "session" dans TOUS tes fichiers .twig
+$twig->addGlobal('session', $_SESSION); 
 
 $page = $_GET['page'] ?? 'accueil';
 
 switch ($page) {
-
     case 'accueil':
         echo $twig->render('index.twig');
         break;
